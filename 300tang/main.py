e6d1bf88c528224d1abeeec8e3c3933ea40b6f4b
yankihue <yankisirtas@hotmail.com>
2021-04-08 19:55:24 +0300

    Add align and color

--- 300tang/main.py
+++ 300tang/main.py
@@ -1,35 +1,35 @@
 from PIL import Image, ImageFont, ImageDraw 
 import json
 import random
+from time import time, sleep
 
-#random poem id
-n = random.randint(0,319)
-print(n)
+while True:
+    #random poem id
+    n = random.randint(0,319)
+    print(n)
 
-with open('poems.json') as f:
-  data = json.load(f)
-
-
-def find_poem (n):
- for keyval in data:
-  if n == keyval['id']:
-   return keyval['contents']
-
-if (find_poem(n) != None):
-  title_text=find_poem(n)
-else:
-  print("Couldn't get poem. Please try running the application again")
+    with open('poems.json') as f:
+        data = json.load(f)
 
 
+    def find_poem (n):
+        for keyval in data:
+            if n == keyval['id']:
+                return keyval['contents']
 
+    if (find_poem(n) != None):
+        title_text=find_poem(n)
 
+    base = Image.open("base.jpeg")
 
-base = Image.open("base.jpeg")
+    image_editable = ImageDraw.Draw(base)
 
-image_editable = ImageDraw.Draw(base)
+    ttf=ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 48)
 
-ttf=ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 64)
+    w,h = image_editable.textsize(title_text, font=ttf)
 
-image_editable.text((0,0), title_text, (237, 230, 211),font=ttf)
+    image_editable.text(((1920-w)/2,(1200-h)/2), title_text, (0, 0, 0,128),font=ttf)
 
-base.save('output.jpeg')
+    base.save('output.jpeg')
+
+    sleep(60 - time() % 60)
